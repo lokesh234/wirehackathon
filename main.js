af40c6d3def971b33db67d340bebb93c6f022a9a
Alyssa Ungerer <alyssaungerer@gmail.com>
2019-11-02 13:42:54 -0400

    letsss goooooooooooo

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ var bird;
 var audio;
 var birdWidth = 30;
 var birdHeight = 30;
+var xPos = 100;
+var yPos = 150;
+
 var cvs = document.getElementById('canvas');
 var ctx = cvs.getContext('2d');
 
+var bird = new Image();
+bird.src = "/hackathon_bird.png"
 
+ctx.drawImage(bird, xPos, yPos, birdWidth, birdHeight);
+
+function draw() {
+    ctx.drawImage(bird, 150, 150);
+}
+
+
+/*
 // bird object
 function start() {
     bird = new Image(birdWidth, birdHeight);
-    bird.src = ''; // Image Here
+    bird.src = 'hackathon_bird.png'; // Image Here
     audio = new Audio();
     audio.src = ''; //Audio Here 
     
@@ -31,6 +44,7 @@ var gameCanvas = {
 
         // update position based on time
 
-        
+
         // detect a collision with pipe and bird
 }
+*/

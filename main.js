c5550cd4cb554f7c3e23d0c9874a3e4baed03ad4
Alyssa Ungerer <alyssaungerer@gmail.com>
2019-11-02 12:43:51 -0400

    Added code layout to main

--- main.js
+++ main.js
@@ -2,11 +2,29 @@ var bird;
 var birdWidth = 30;
 var birdHeight = 30;
 
+
+// bird object
 function start() {
     bird = new Image(birdWidth, birdHeight);
-    bird.src = '';
+    bird.src = ''; // Image Here
+    
 }
 
-var gameCanvas = {
+// function for moving left and right with bounds
+function birdMove() {
+    // event listener
+}
 
+// game background
+var gameCanvas = {
+    canvas : function() {
+        // Set canvas values
+    }
+
+    // generate pipes
+
+        // update position based on time
+
+        
+        // detect a collision with pipe and bird
 }
